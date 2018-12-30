339fd9ad0700f5f54ce4fddc7564ac66538309ce
Wojtek Niedzielski <nwojtek@gmail.com>
2018-12-31 00:04:08 +0100

    nic juz nie dziala

--- zal2.py
+++ zal2.py
@@ -2,7 +2,7 @@
 import socket
 import struct
 import _thread
-
+from threading import Thread
 #classes block
 class nickBase:
     usersBase = {}
@@ -19,13 +19,42 @@ class nickBase:
     def addNick(self, nick, addr):
         self.usersBase[nick] = addr
 
+class mCastListen(Thread):
+    multicast_group = '224.1.1.1'
+    server_address = ('', 10000)
+    dataRcv = ''
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+    def __init__(self):
+        Thread.__init__()
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind(self.server_address)
+        group = socket.inet_aton(self.multicast_group)
+        mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+        self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+    def listen(self):
+        while True:
+            data, address = self.sock.recvfrom(1024)
+            self.encSliDat(data)
+            self.prnData()
+            sock.sendto(bytes('ack', 'utf8'), address)
+        print("Egzit!")
+    def encSliDat(self,mess):
+        self.dataRcv = mess.decode('utf8').split()
+    def prnData(self):
+        print(" ".join(self.dataRcv))
+
 #global vars
-multicast_group = '224.1.1.1'
-server_address = ('', 10000)
-multicast_addr = ('224.1.1.1',1000)
+
+multicast_addr = ('224.1.1.1',10000)
 nickB = nickBase()
 
 #func block
+
+#data decoder and splitter
+def encSliDat(message):
+    return message.decode('utf8').split()
 
 #Multicast listener
 def mCastListener():
@@ -46,29 +75,26 @@ def mCastListener():
     # Listener main loop
     while True:
         data, address = sock.recvfrom(1024)
-        print(data.decode('utf8'))
+        message = encSliDat(data)
+        print(message)
         sock.sendto(bytes('ack','utf8'), address)
     print("Egzit!")
 
 #multicast sender
 def mCastSender(mess):
-    print(mess)
     mess = bytes(mess,'utf8')
-    multicast_group = ('224.1.1.1', 10000)
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
     sock.settimeout(1)
 
     ttl = struct.pack('b', 1)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
     try:
-        sent = sock.sendto(mess, multicast_group)
+        sent = sock.sendto(mess, multicast_addr)
 
         # Look for responses from all recipients
         while True:
-            #print('waiting to receive')
             try:
                 data, server = sock.recvfrom(16)
             except socket.timeout:
@@ -92,7 +118,7 @@ def cli():
             _thread.exit()
             break
         elif command[0].lower() == "listen":
-            _thread.start_new_thread(mCastListener,())
+            _thread.start_new_thread(mCastListen,())
         elif command[0].lower() == "send":
             s = " ".join(command[1::])
             _thread.start_new_thread(mCastSender,(s,))
