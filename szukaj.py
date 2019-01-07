20ea0d53ce5794f36517afd90ee8c2f47b202517
Wojtek Niedzielski <nwojtek@gmail.com>
2019-01-08 00:01:25 +0100

    dziala wysowanie punktow! i wyznacza jakas droge!

--- szukaj.py
+++ szukaj.py
@@ -3,6 +3,7 @@ import math as m
 import matplotlib.pyplot as plt
 #globals
 punkty = []
+wynik = []
 
 #miasta
 class miasta:
@@ -31,24 +32,41 @@ def tabCr():
             else:
                 dist = m.sqrt(m.pow(m.fabs(p.wspY - o.wspY),2)+m.pow(m.fabs(p.wspX - p.wspX),2))
                 print("Nr: " + str(o.nrMiasta) +" to Nr: "+ str(p.nrMiasta)+ ", DIST: "+str(dist))
-def plotCr():
+def plotCr(pkts):
     plt.title("Punkty do odwiedzenia")
     plt.xlabel("X")
     plt.ylabel("Y")
     X = []
     Y = []
-    for o in punkty:
+    for o in pkts:
         X.append(o.wspX)
         Y.append(o.wspY)
         plt.annotate(str(o.nrMiasta),(o.wspX,o.wspY))
-    #plt.plot(X,Y)
-    plt.scatter(X,Y)
+    plt.plot(X,Y)
+    #plt.scatter(X,Y)
     plt.grid()
     plt.show()
+
+def engine(czyPierwszy):
+    if czyPierwszy:
+        wynik.append(punkty[0])
+        del punkty[0]
+    distTmp = []
+    for o in punkty:
+        distTmp.append(m.sqrt(m.pow((o.wspY - wynik[len(wynik) -1].wspY),2) + m.pow((o.wspX - wynik[len(wynik) -1].wspX),2)))
+    tmpMin = min(distTmp)
+    wynik.append(punkty[distTmp.index(tmpMin)])
+    del punkty[distTmp.index(tmpMin)]
+    if punkty:
+        engine(False)
+
 
 generatorMiast(15)
 for o in punkty:
     o.allPrt()
-tabCr()
-plotCr()
+#tabCr()
+engine(True)
+for o in wynik:
+    o.allPrt()
+plotCr(wynik)
 
