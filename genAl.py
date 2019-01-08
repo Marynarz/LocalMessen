78a18c99f077bd26c712ba34c06fa2228876a2bb
Wojtek Niedzielski <nwojtek@gmail.com>
2019-01-08 23:07:26 +0100

    dodane peracje bitoowe

--- genAl.py
+++ genAl.py
@@ -5,7 +5,9 @@ wyniki = []
 
 #sprawdzanie dopasowania
 def sprDop(x):
+    #print(x)
     if x <1 or x >127:
+        del pBadana[pBadana.index(x)]
         return 0
     return 2 * (pow(x,2) + 1)
 
@@ -13,11 +15,49 @@ def sprDop(x):
 def genP(x):
     for i in range(0,x):
         pBadana.append(random.randint(1,127))
+    pBadana.sort()
+
+#mutacja
+def mutant(x):
+    mutantGen = 1<< random.randint(0,7) #1 przesuwamy bitowo, losowa pomiedzy najstarszym a najmlodszym bitem
+    print("Mutant gen: " + str(mutantGen))
+    return x^mutantGen #exclusive or
+
+#krzyzowanie
+def cross(x,y):
+    locus = 7 - random.randint(0,7)
+    #tworzenie maski bitowej d przneoseznia loculusa
+    bMask = 0
+    for i in range(0,locus):
+        bMask = bMask | 1<< i
+    #wyluskanie elemntu do zmiany
+    tempX = x & bMask
+    tempY = y & bMask
+    negBMask = ~bMask
+    headX = x & negBMask
+    headY = y & negBMask
+
+    sumX = headX | tempY
+    sumY = headY | tempX
+    print(x)
+    print(sumX)
+
+
+#sterwanie mutacjami i kombinacjami
+def mutSteer():
+    for nbs, p in enumerate(pBadana):
+        pBadana[nbs] = mutant(p)
 
 #let's start
 def engine():
-    genP(10)
-    for i in range(0,10):
-        print(str(sprDop(pBadana[i])))
+    genP(5)
+    for i in range(1,20):
+        print("Nowa generacja " + str(i))
+        genP(10 - len(pBadana))
+        for probka in pBadana:
+            print(str(sprDop(probka)))
+        del pBadana[0:5]
+        mutSteer()
+        print("-------------------------")
 
 engine()
