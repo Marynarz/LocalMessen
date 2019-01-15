c61705947249e74eff2354b88dfe7b2e749c324c
Wojtek Niedzielski <nwojtek@gmail.com>
2019-01-15 23:10:33 +0100

    koniec!

--- zal2.py
+++ zal2.py
@@ -61,9 +61,13 @@ class RoomHandler:
     def checkAval(self):
         sender.setMess("ROOM AVAL")
         sender.run()
-        tmp = sender.getAck().split()
+        tmp = sender.getAck()
         for r in tmp:
             self.avaliableRooms.append(r)
+
+    def addRoom(self,room):
+        if room not in self.avaliableRooms:
+            self.avaliableRooms.append(room)
 
     def roomsAval(self):
         if "ack" in self.avaliableRooms:
@@ -89,7 +93,8 @@ class RoomHandler:
         return self.activeRoom
 
     def whoIs(self):
-        pass
+        sender.setMess("WHOIS "+self.activeRoom)
+        sender.run()
 
 #listener
 class MCastListen(threading.Thread):
@@ -115,7 +120,7 @@ class MCastListen(threading.Thread):
                 break
             data, address = self.sock.recvfrom(1024)
             self.encSliDat(data)
-            if not data == sender.mess or self.dataRcv[0] == 'NICK':
+            if not data == sender.mess or self.dataRcv[0] in ['NICK','WHOIS']:
                 self.addrRcv = address
                 self.sock.sendto(bytes(self.messValidatin(), 'utf8'), address)
         print("Bye!")
@@ -138,12 +143,17 @@ class MCastListen(threading.Thread):
         elif self.dataRcv[0] == "JOIN" and self.dataRcv[1] == rooms.getActiveRoom():
             print('\x1b[1;31;40m' + self.dataRcv[2]+ " join room!" + '\x1b[0m')
             result = 'ack'
+        elif self.dataRcv[0] == "JOIN" and self.dataRcv[1] != rooms.getActiveRoom():
+            rooms.addRoom(self.dataRcv[1])
+            result = 'ack'
         elif self.dataRcv[0] == "LEFT" and self.dataRcv[1] == rooms.getActiveRoom():
             print('\x1b[1;31;40m' + self.dataRcv[2] + " leave room!" + '\x1b[0m')
             result = 'ack'
         elif self.dataRcv[0] == 'MSG' and self.dataRcv[2] == rooms.getActiveRoom():
             print('\x1b[1;32;40m' + self.dataRcv[1] + ": " + " ".join(self.dataRcv[3::]) + '\x1b[0m')
             result = 'ack'
+        elif self.dataRcv[0] =="WHOIS" and self.dataRcv[1] ==rooms.getActiveRoom():
+            result = " ".join(["ROOM",rooms.getActiveRoom(),nickB.getSelfNick()])
         else:
             result = 'ack'
         return result
@@ -152,12 +162,13 @@ class MCastListen(threading.Thread):
 class MCastSend(threading.Thread):
     mess = ""
     multicast_addr = ('224.1.1.1', 10000)
-    ack = ""
+    ack = []
 
     def __init__(self):
         threading.Thread.__init__(self)
 
     def run(self):
+        self.ack.clear()
         try:
             sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             sock.settimeout(1)
@@ -172,17 +183,21 @@ class MCastSend(threading.Thread):
                 except socket.timeout:
                     break
                 else:
-                    self.ack = data.decode('utf8')
+                    for a in data.decode('utf8').split():
+                        if a not in self.ack:
+                            self.ack.append(a)
                     datatmp = data.decode('utf8').split()
                     if datatmp[0] == 'NICK' and datatmp[2] =='BUSY':
                         print('\x1b[1;31;40m' + "Nick busy!" + '\x1b[0m')
                         return False
         finally:
             sock.close()
+            if "ROOM" in self.ack:
+                print(" ".join(self.ack[2::]))
         return True
 
     def setMess(self,mess):
-        if mess.split()[0] in ["JOIN","NICK","ROOM","LEFT"]:
+        if mess.split()[0] in ["JOIN","NICK","ROOM","LEFT","WHOIS"]:
             self.mess = bytes(mess,'utf8')
         else:
             self.mess = bytes(" ".join(["MSG",nickB.getSelfNick(),rooms.getActiveRoom(),mess]),'utf8') #'MSG ' + nickB.getSelfNick() + " " + rooms.getActiveRoom()+ " " + mess, 'utf8')
@@ -199,13 +214,15 @@ rooms = RoomHandler()
 
 #func block
 def help():
-    print("Chat multicastwy dla sieci lokalnej")
+    print("Chat multicastowy dla sieci lokalnej")
     print("-----------------------------------")
     print("Komendy:")
     print(" : listen - odpalenie nasluchu, !!! Komenda nie dziala !!!")
     print(" : send <wiadomosc> - wysylanie wiadomosci")
     print(" : help - odpalenie tej pomocy")
     print(" : exit - wyjscie z programu")
+    print(" : left - wyjscie z pokoju")
+    print(" : whois - pokazuje kto jest w aktualnym pokoju")
     print()
     print("Funkcje:")
     print(" : nasluch - nasluch startuje w momencie dpalenia programu i trwa do jego zamkniecia,")
@@ -243,6 +260,8 @@ def cli():
             help()
         elif command[0].lower() == "left":
             rooms.leftRoom(False)
+        elif command[0].lower() =="whois":
+            rooms.whoIs()
         else:
             print("Wrong command!")
 
